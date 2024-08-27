e5f0c27fdfd5d82850daddc7a64f6e52fc8c1f03
Pedro-Henrique-RL <pedroh130702@gmail.com>
2024-08-27 20:20:29 -0300

    Create exercicio 1.py

---
+++ Semana 2/exercicio 1.py
@@ -0,0 +1,4 @@
+preco_fabrica = float(input("digite o preço de fabricá da peça: R$"))
+percentual_acrescimo = 30
+valor_final = preco_fabrica * (1 + percentual_acrescimo / 100)
+print(" O valor final da peça é: R$", valor_final)
