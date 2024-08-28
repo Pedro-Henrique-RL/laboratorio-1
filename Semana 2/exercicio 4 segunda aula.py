3b04a975ef4daaa766e7030a3e9ccf1fdc769f6d
Pedro-Henrique-RL <pedroh130702@gmail.com>
2024-08-27 22:20:54 -0300

    Create exercicio 4 segunda aula.py

---
+++ Semana 2/exercicio 4 segunda aula.py
@@ -0,0 +1,8 @@
+precoDaGasolina = 6.20
+print("O preço da gasolina é:", precoDaGasolina)
+
+gasolinaAbastecida = int(input("Quanto de gasolina? "))
+
+valorPago = gasolinaAbastecida / precoDaGasolina
+
+print("Gasolina que será abastecida:", valorPago)
